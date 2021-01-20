68ce1fd0dc73db15dfede6ef01a8370af9720c7a
vanderFlierFilip <f.vanderflier@gmail.com>
2021-01-20 01:06:45 +0100

    Added code in scraper for pagination

--- scraper/tinex_scraper.py
+++ scraper/tinex_scraper.py
@@ -2,17 +2,26 @@ from bs4 import BeautifulSoup
 import requests
 
 
-html_text = requests.get('https://www.e-tinex.mk/default.aspx?b=31&p=1&f5=0&&i=96').text 
-soup = BeautifulSoup(html_text, 'lxml')
-
-proizvodi = soup.find_all('div', class_ = 'block block_6')
 
 
-for proizvod in proizvodi:
-    proizvod_naziv = proizvod.find('div', class_ = 'desc_grid1').text 
-    proizvod_cena = proizvod.find('div', class_ = 'price_cont').text.replace(' ','')
+
+for x in range(1,13):
     
-    print(f"Производ: {proizvod_naziv.strip()}")
-    print(f"Цена: {proizvod_cena.strip()}")
+    url = ('https://www.e-tinex.mk/default.aspx?b=31&p=0')
+   
+    html_text = requests.get(url+str(x)).text
+    soup = BeautifulSoup(html_text, 'lxml')
     
-    print('')
+    proizvodi = soup.find_all('div', class_ = 'block block_6')
+    
+    for proizvod in proizvodi:
+        proizvod_naziv = proizvod.find('div', class_ = 'desc_grid1').text 
+        proizvod_cena = proizvod.find('div', class_ = 'price_cont').text.replace('\r\n', '').replace('\xa0', '').replace(' ', '')
+        
+        
+        print(f"Производ: {proizvod_naziv.strip()}")    
+        print(f"Цена: {proizvod_cena.strip()}")    
+           
+        
+
+        
